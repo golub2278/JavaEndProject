689a51b81a5aae10e238ab92b1c1b12d9b0a9810
golub2278 <golubbs9@gmail.com>
2026-04-10 19:44:21 +0300

    PJ

---
+++ byMaksim/src/main/java/JavaProject/byMaksim/ByMaksimApplication.java
@@ -0,0 +1,12 @@
+package JavaProject.byMaksim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByMaksimApplication {
+
+	public static void main(String[] args) {
+
+	}
+}
